2afba2b681a6704969eb7e2a53e1967859b4f50a
ananyarao28 <aprao@ualberta.ca>
2021-09-17 10:50:28 -0600

    add files

---
+++ multi_proxy_server.py
@@ -0,0 +1,67 @@
+import socket,time,sys
+from multiprocessing import Process
+
+HOST = ""
+PORT = 1027
+BUFFER_SIZE = 1024
+
+def get_remote_ip(host):
+    print(f'Getting IP for {host}')
+    try:
+        remote_ip = socket.gethostbyname( host )
+    except socket.gaierror:
+        print ('Hostname could not be resolved. Exiting')
+        sys.exit()
+
+    print (f'Ip address of {host} is {remote_ip}')
+    return remote_ip
+
+def handle_request(addr,conn):
+    print("Connected by", addr)
+
+    full_data = conn.recv(BUFFER_SIZE)
+    print(full_data)
+    conn.sendall(full_data)
+    conn.shutdown(socket.SHUT_RDWR)
+    conn.close()
+
+def main():
+    extern_host = 'www.google.com'
+    port = 80
+
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as proxy_start:
+        print("Starting proxy server")
+        proxy_start.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        proxy_start.bind((HOST,PORT))
+        proxy_start.listen(1)
+
+        while True:
+            conn,addr = proxy_start.accept()
+            print("Connected by", addr)
+
+            with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as proxy_end:
+                remote_ip = get_remote_ip(extern_host)
+                proxy_end.connect((remote_ip, port))
+
+                p = Process(target=handle_request,args=(addr,conn))
+                p.daemon = True
+                p.start()
+                print("Started process ", p)
+                
+                #send_full_data = conn.recv(BUFFER_SIZE)
+                #print(f"Sending received data {send_full_data} to google")
+                #proxy_end.sendall(send_full_data)
+
+                #proxy_end.shutdown(socket.SHUT_WR)
+
+              #  data = proxy_end.recv(BUFFER_SIZE)
+             #   print(f"Sending received data {data} to client")
+            #    proxy_end.shutdown(socket.SHUT_WR)
+           #     conn.send(data) 
+            
+                
+            conn.close()
+
+if __name__ == "__main__":
+    main()
+            
